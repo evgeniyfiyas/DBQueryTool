3a899fb2f58b4c83e919e1bf5726b4580380c495
evgeniyfiyas <evgeniyfiyas@gmail.com>
2019-06-18 04:07:21 +0300

    added models for database generation

---
+++ DBQueryTool.Models/Report.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBQueryTool.Models
+{
+    class Report
+    {
+        public int Id { get; set; }
+        public string SerializedReport { get; set; }
+        public int UserId { get; set; }
+        public int TemplateId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ DBQueryTool.Models/Template.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBQueryTool.Models
+{
+    class Template
+    {
+        public int Id { get; set; }
+        public string SerializedTemplate { get; set; }
+        public int TypeId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
