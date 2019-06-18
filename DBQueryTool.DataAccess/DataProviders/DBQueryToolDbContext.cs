9b1de60235d1f01b5dbeec73ee996a6894e580a3
evgeniy <evgeniyfiyas@gmail.com>
2019-06-18 15:35:50 +0300

    created dbcontext class

---
+++ DBQueryTool.DataAccess/DataProviders/DBQueryToolDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBQueryTool.DataAccess.Models;
+
+namespace DBQueryTool.DataAccess.DataProviders
+{
+    public class DBQueryToolDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Template> Templates { get; set; }
+        public DbSet<Report> Reports { get; set; }
+        public DbSet<TemplateType> TemplateTypes { get; set; }
+    }
+}
